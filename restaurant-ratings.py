69aec418bdc721fe783023fef9736e90cbc8a9ec
Rosie <missjennchen+01@gmail.com>
2016-04-11 15:56:49 -0700

    We have completed the required assignment

--- restaurant-ratings.py
+++ restaurant-ratings.py
@@ -1,6 +1,8 @@
 # your code goes here
 import sys # open the file 
 
+#define a list
+rest_list = []
 file_name = sys.argv[1]
 file_data = open(file_name) # iterate and split data
 
@@ -8,6 +10,11 @@ for line in file_data: #take the newlines off
 
 	strip = line.rstrip()
 	rest_rating = strip.split(":")
+	rest_list.append(rest_rating)
 
-	rest, rating = rest_rating
+rest_list.sort()
+	
+for item in rest_list:
+	rest, rating = item
+	#append to list
 	print "%s is rated at %s." % (rest, rating)
