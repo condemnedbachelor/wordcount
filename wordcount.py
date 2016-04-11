a9d091c8dafc43bbe7e2f84b642b560a1acd73fb
Rosie <missjennchen+01@gmail.com>
2016-04-11 12:48:07 -0700

    removed punctuation, uniform case, allowed for file append when running script

--- wordcount.py
+++ wordcount.py
@@ -1,44 +1,25 @@
 # put your code here.
 # Find each unique word
-#intialize empty list
-word_count_list = []
+import string
+import sys
 # make an empty dict
 wordcount = {}
 # open the file
-file_data = open("twain.txt")
-# take off the new line 
+file_data = open(sys.argv[1])
 # iterate through the lines
 for line in file_data:
+# take off the new line 
 	strip = line.rstrip()
 # split by space
 	words = strip.split(" ")
-# turn it all into list
 	for item in words:
 # for each item in wordcount, add one!
 #		for item in wordcount:
-		value = wordcount.get(item, 0) + 1
-		wordcount[item] = value
+		lower_item = item.lower()
+		lower_item = lower_item.translate(None, string.punctuation)
+		value = wordcount.get(lower_item, 0) + 1
+		wordcount[lower_item] = value
 for k, v in wordcount.iteritems():
 	print k, v
 
 
-# setify the list
-#unique_words = set(word_count_list)
-
-# keys in our dictionary first set dummy value
-#for item in unique_words:
-#	wordcount[item] = 0
-# somewhere counting happens
-# we have a list with all of the words
-# dictionary with unique
-#for item in word_count_list:
-#	value = value + 1
-
-# keys are from the unique set return which is now our dict
-# values will come from the list since it has the repeat values
-#for key, value in wordcount.iteritems():
-#	value 
-#	print key, value
-#	value = 
-#	wordcount[key] = #whatever number
-# values number of times 
