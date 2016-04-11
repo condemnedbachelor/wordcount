500d40aafa5e2d427f2927b00e01a3156717a7ff
Rosie <missjennchen+01@gmail.com>
2016-04-11 12:22:44 -0700

    we did it exclamation point

--- wordcount.py
+++ wordcount.py
@@ -14,11 +14,31 @@ for line in file_data:
 	words = strip.split(" ")
 # turn it all into list
 	for item in words:
-		word_count_list.append(item)
+# for each item in wordcount, add one!
+#		for item in wordcount:
+		value = wordcount.get(item, 0) + 1
+		wordcount[item] = value
+for k, v in wordcount.iteritems():
+	print k, v
+
+
 # setify the list
-print word_count_list
-# keys in our dictionary first set dummy values
+#unique_words = set(word_count_list)
 
-
+# keys in our dictionary first set dummy value
+#for item in unique_words:
+#	wordcount[item] = 0
 # somewhere counting happens
+# we have a list with all of the words
+# dictionary with unique
+#for item in word_count_list:
+#	value = value + 1
+
+# keys are from the unique set return which is now our dict
+# values will come from the list since it has the repeat values
+#for key, value in wordcount.iteritems():
+#	value 
+#	print key, value
+#	value = 
+#	wordcount[key] = #whatever number
 # values number of times 
