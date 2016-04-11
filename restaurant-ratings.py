07e73d881965e00fce8bd496ef8820ed7824d4c3
Rosie <missjennchen+01@gmail.com>
2016-04-11 16:29:16 -0700

    you can now update restaurant ratings ad inifinitum

--- restaurant-ratings.py
+++ restaurant-ratings.py
@@ -1,10 +1,11 @@
 # your code goes here
 import sys # open the file 
-
+import random
 
 rest_dict = {} #define a dict
 file_name = sys.argv[1]
 file_data = open(file_name) # iterate and split data
+new_rating = 0
 
 for line in file_data: #take the newlines off
 
@@ -20,3 +21,20 @@ sorted_dict = sorted(rest_dict)
 
 for x in rest_dict:
 	print "%s is rated at %s." % (x, rest_dict[x])
+
+
+#Greet the user and ask for their name
+user_name = raw_input("Hi! What's your name? >>> ")
+#Choose a random restaurant from the list in the file
+while new_rating != "q":
+	random_rest = random.choice(rest_dict.items())
+	rest, rate = random_rest
+	#Tell the user the chosen restaurant and its rating
+	print "%s is rated at %s." % (rest, rate)
+	#Ask the user what the new rating should be
+	new_rating = raw_input("What should the new rating be? >>> ")
+	#Update the rating
+	rest_dict[rest] = new_rating
+#Choose another restaurant, repeat steps 2-4
+
+#Continue until the user types q.
